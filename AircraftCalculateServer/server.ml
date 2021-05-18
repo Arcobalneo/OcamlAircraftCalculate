86564f3494b87aab956effc72e2ca3116856a881
Arcobalneo <137518538@qq.com>
2021-05-18 18:38:20 +0800

    Server删掉求行列式

--- AircraftCalculateServer/server.ml
+++ AircraftCalculateServer/server.ml
@@ -1,20 +1,7 @@
 open Lwt
 open Cohttp
 open Cohttp_lwt_unix
-open Yojson
-open Deter
 open Json_handler
-let rec listify (json):int list list = 
-  let rec intify (list):int list = 
-    match list with 
-    | [] -> []
-    | (`Int x)::rest -> x::(intify rest)
-    | _ -> raise (Json_error "Does not support in intify")
-  in match json with 
-     | [] -> []
-     | (`List l)::rest -> (intify l)::(listify rest)
-     | _ -> raise (Json_error "Does not support in listify")
-;;
 
 let server =
   let callback _conn req body =
@@ -25,9 +12,7 @@ let server =
       Printf.eprintf "\nUri: %s\nMethod: %s\nHeaders: %s Body: %s" uri meth headers body;
       flush stderr;
       let body_json = Yojson.Basic.from_string body in
-      match body_json with
-      | (`List l) -> let m = (listify l) in let d = (determinant m) in (Printf.sprintf "%i\n" d)
-      | _ -> let s = jsonproc body_json in (Printf.sprintf "%s\n" s)
+      let s = jsonproc body_json in (Printf.sprintf "%s\n" s)
     )
     >>= fun body -> Server.respond_string ~status:`OK ~body ()
   in
